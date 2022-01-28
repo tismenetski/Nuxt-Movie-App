1ddc94176068a1f53a8519a06d7bf20def865cf3
tismenetski <tismenetski@gmail.com>
2022-01-28 15:44:50 +0200

    Created Store

---
+++ store/index.js
@@ -0,0 +1,17 @@
+export const state = () => ({
+  genres : {}
+})
+
+export const mutations = {
+  populateGenres(state,payload) {
+    state.genres = payload;
+  }
+}
+
+export const getters = {
+
+}
+
+export const actions = {
+
+}
